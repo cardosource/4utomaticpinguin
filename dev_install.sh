426ced818e1e38f12eb34a7af5c6300df2595835
jeferson cardoso <jefersoncardoso.dev@gmail.com>
2025-02-27 00:05:12 -0400

    Update dev_install.sh
    corrigido versão e instalação

--- dev_install.sh
+++ dev_install.sh
@@ -4,57 +4,55 @@
 # 2 - descompactar
 # 3 - configurar
 
-
 if [ -n "$SUDO_USER" ]; then
   usuario=$SUDO_USER
 else
   usuario=$USER
 fi
 
-
-declare -a nodeJs
 declare -a downloads
 downloads[0]="https://nodejs.org/dist/v22.14.0/node-v22.14.0-linux-x64.tar.xz" # Stable Releases
+mkdir -p /tmp/temp
 
-
-# baixar arquivos.
 for ((i = 0; i < ${#downloads[@]}; i++)); do
   echo "[ $i ] - Baixando: ${downloads[i]}"
   wget ${downloads[i]} --directory-prefix=/tmp/temp > /dev/null 2>&1
 done
 
-# descompactar e remover
-if test -e /tmp/temp/*.tar.xz; then
-  tar -xJvf /tmp/temp/*.tar.xz -C /tmp/temp > /dev/null 2>&1
-  rm /tmp/temp/*.tar.xz
+if ls /tmp/temp/*.tar.xz > /dev/null 2>&1; then
+  for arquivo in /tmp/temp/*.tar.xz; do
+    echo "Descompactando $arquivo..."
+    tar -xJvf "$arquivo" -C /tmp/temp > /dev/null 2>&1
+    rm "$arquivo"
+  done
 fi
 
 sleep 5
-# movendo arquivo
+
 for dir in /tmp/temp/*; do
-  export package=${dir:10}
+  package=$(basename "$dir")
   echo "Pacote $package baixado"
-  mv -v "$dir"/ /usr/local/bin/ > /dev/null 2>&1
+  mv -v "$dir" /usr/local/bin/ > /dev/null 2>&1
 done
 
 rm -rf /tmp/temp/
 
 sleep 10
-for lCal in /usr/local/bin/*; do
-  pasta=${lCal:15:30}
-  pasta=${pasta%%*( )}
-  if [ "$pasta" = "$package" ]; then
-   sleep 5
-     echo "#NodeJs setup dev" >> /home/$usuario/.bashrc
-     echo "export NODEJS_HOME=/usr/local/bin/$package" >> /home/$usuario/.bashrc
-     echo "export PATH=$PATH:$NODEJS_HOME/bin" >> /home/$usuario/.bashrc
-
-
-    sleep 5
+echo "Configurando variáveis de ambiente..."
+if [ -d "/usr/local/bin/$package" ]; then
+  if ! grep -q "#NodeJs setup dev" /home/$usuario/.bashrc; then
+    echo "#NodeJs setup dev" >> /home/$usuario/.bashrc
+    echo "export NODEJS_HOME=/usr/local/bin/$package" >> /home/$usuario/.bashrc
+    echo 'export PATH=$PATH:$NODEJS_HOME/bin' >> /home/$usuario/.bashrc
+    echo "Variáveis de ambiente configuradas para $package."
+  else
+    echo "As variáveis de ambiente já estão configuradas no .bashrc."
   fi
-done
-
+else
+  echo "Erro: Diretório do pacote não encontrado em /usr/local/bin/."
+  exit 1
+fi
 
 source /home/$usuario/.bashrc
 
-echo "$package Instalado com sucesso"
+echo "$package instalado com sucesso!"
