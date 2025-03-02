22eb8f3df6fe828cc5219635967820966e7882d1
jeferson cardoso <jefersoncardoso.dev@gmail.com>
2025-03-01 20:26:06 -0400

    Update dev_install.sh

--- dev_install.sh
+++ dev_install.sh
@@ -10,18 +10,19 @@ else
   usuario=$USER
 fi
 
+declare -a nodeJs
 declare -a downloads
 downloads[0]="https://nodejs.org/dist/v22.14.0/node-v22.14.0-linux-x64.tar.xz" # Stable Releases
-mkdir -p /tmp/temp
 
+# baixar arquivos.
 for ((i = 0; i < ${#downloads[@]}; i++)); do
   echo "[ $i ] - Baixando: ${downloads[i]}"
   wget ${downloads[i]} --directory-prefix=/tmp/temp > /dev/null 2>&1
 done
 
+# Verificar se há arquivos .tar.xz e descompactar
 if ls /tmp/temp/*.tar.xz > /dev/null 2>&1; then
   for arquivo in /tmp/temp/*.tar.xz; do
-    echo "Descompactando $arquivo..."
     tar -xJvf "$arquivo" -C /tmp/temp > /dev/null 2>&1
     rm "$arquivo"
   done
@@ -29,30 +30,29 @@ fi
 
 sleep 5
 
+# movendo arquivo
 for dir in /tmp/temp/*; do
-  package=$(basename "$dir")
-  echo "Pacote $package baixado"
-  mv -v "$dir" /usr/local/bin/ > /dev/null 2>&1
+  export package=${dir:10}
+   echo "Pacote $package baixado"
+   mv -v "$dir"/ /usr/local/bin/ > /dev/null 2>&1
 done
 
 rm -rf /tmp/temp/
 
 sleep 10
-echo "Configurando variáveis de ambiente..."
-if [ -d "/usr/local/bin/$package" ]; then
-  if ! grep -q "#NodeJs setup dev" /home/$usuario/.bashrc; then
+echo "Configurando variaveis de ambiente"
+for lCal in /usr/local/bin/*; do
+  pasta=${lCal:15:30}
+  pasta=${pasta%%*( )}
+  if [ "$pasta" = "$package" ]; then
+   sleep 5
     echo "#NodeJs setup dev" >> /home/$usuario/.bashrc
     echo "export NODEJS_HOME=/usr/local/bin/$package" >> /home/$usuario/.bashrc
     echo 'export PATH=$PATH:$NODEJS_HOME/bin' >> /home/$usuario/.bashrc
-    echo "Variáveis de ambiente configuradas para $package."
-  else
-    echo "As variáveis de ambiente já estão configuradas no .bashrc."
+   sleep 5
   fi
-else
-  echo "Erro: Diretório do pacote não encontrado em /usr/local/bin/."
-  exit 1
-fi
+done
 
 source /home/$usuario/.bashrc
 
-echo "$package instalado com sucesso!"
+echo "$package Instalado com sucesso"
